35c7c82f80dace6c4f0bb11a264354cef86b7e01
Carlos <carlosaguinaga01@gmail.com>
2021-09-22 00:43:52 -0500

    fix: particle fixed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+
 particlesJS("particles-js", {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
+    number: { value: 60, density: { enable: true, value_area: 800 } },
+    color: { value: "#f7df1e" },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
@@ -14,20 +15,20 @@ particlesJS("particles-js", {
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
     },
     size: {
-      value: 3,
+      value: 6,
       random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
     },
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#ffffff",
-      opacity: 0.4,
+      color: "#f7df1e",
+      opacity: 0.5,
       width: 1,
     },
     move: {
       enable: true,
-      speed: 6,
+      speed: 2,
       direction: "none",
       random: false,
       straight: false,
